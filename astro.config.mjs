8d6d06afe0fe79f8addffbaf7f6243722a69ebeb
robrrto <robertoclc@gmail.com>
2026-08-26 12:22:41 -0300

    Atualiza imagem da página de vendas

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://scientiaessencial.com.br',
+  output: 'server', // ou 'hybrid' / 'static' dependendo do seu projeto
+  adapter: cloudflare(),
   integrations: [sitemap()],
 });
